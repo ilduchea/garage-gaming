78c40adac9a7f49ae8524988c2d81216d19d95f5
Ilduchea <ilduchea@gmail.com>
2017-06-16 10:17:40 -0700

    add routing to base pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+import { GamersComponent } from './gamers/gamers.component';
+import { GamerDetailComponent } from './gamer-detail/gamer-detail.component';
+import { NewGamerComponent } from './new-gamer/new-gamer.component';
+import { EditGamerComponent } from './edit-gamer/edit-gamer.component';
+
 const routes: Routes = [
   {
     path: '',
-    children: []
+    children: [],
+    component: WelcomeComponent
+  },
+  {
+    path: 'about',
+    children: [],
+    component: AboutComponent
+  },
+  {
+    path: 'gamers',
+    children: [],
+    component: GamersComponent
+  },
+  {
+    path: 'gamers/:id',
+    children: [],
+    component: GamerDetailComponent
+  },
+  {
+    path: 'admin',
+    children: [],
+    component: AdminComponent
   }
 ];
 
@@ -12,4 +41,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
